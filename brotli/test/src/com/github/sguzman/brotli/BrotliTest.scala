0ebde5510e8e918795063adb5607becfb3938ba2
Salvador Guzman <guzmansalv@gmail.com>
2018-03-31 14:49:41 -0700

    added another test case

--- brotli/test/src/com/github/sguzman/brotli/BrotliTest.scala
+++ brotli/test/src/com/github/sguzman/brotli/BrotliTest.scala
@@ -5,6 +5,13 @@ import org.scalatest.FlatSpec
 class BrotliTest extends FlatSpec {
 
   "A computer" should "have brotli installed" in {
-    Brotli.compress("{}")
+    Brotli.compress("some string here")
+  }
+
+  "Brotli" should "be able to undo compression" in {
+    val str = "Another string that can be uncompressed"
+    val compressed = Brotli.compress("Another string that can be uncompressed")
+    val decompressed = Brotli.decompress(compressed)
+    assert(decompressed == str, s"$str =?= $decompressed")
   }
 }
