de79f2fa7e5e51d1b999d62d4d62ea11c03fae8d
Salvador Guzman <guzmansalv@gmail.com>
2018-03-31 14:05:11 -0700

    comment and imports

--- src/main/scala/com/github/sguzman/brotli/Brotli.scala
+++ src/main/scala/com/github/sguzman/brotli/Brotli.scala
@@ -1,7 +1,12 @@
 package com.github.sguzman.brotli
 
 import java.io.{DataInputStream, DataOutputStream}
+import scribe
 
+import scala.sys.process._
+
+// Thanks to some stackoverflow answer i found
+// will put it here when i find it
 object Brotli {
   def compress(s: String): Array[Byte] = {
     scribe.info("Compressing with Brotli...")
